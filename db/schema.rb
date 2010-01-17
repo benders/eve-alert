fc4d45fa925a49de1c178503731bb2787f6dc59a
Nic Benders <nic.benders@gmail.com>
2010-01-17 15:30:46 -0800

    Add BackgrounDRb plugin

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100117055315) do
+ActiveRecord::Schema.define(:version => 20100117232943) do
 
   create_table "accounts", :force => true do |t|
     t.string   "full_api_key"
@@ -17,6 +17,26 @@ ActiveRecord::Schema.define(:version => 20100117055315) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "error"
+  end
+
+  create_table "bdrb_job_queues", :force => true do |t|
+    t.text     "args"
+    t.string   "worker_name"
+    t.string   "worker_method"
+    t.string   "job_key"
+    t.integer  "taken"
+    t.integer  "finished"
+    t.integer  "timeout"
+    t.integer  "priority"
+    t.datetime "submitted_at"
+    t.datetime "started_at"
+    t.datetime "finished_at"
+    t.datetime "archived_at"
+    t.string   "tag"
+    t.string   "submitter_info"
+    t.string   "runner_info"
+    t.string   "worker_key"
+    t.datetime "scheduled_at"
   end
 
   create_table "characters", :force => true do |t|
