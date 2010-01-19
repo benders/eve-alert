db6780cb82dd3b70c70fb210088539f5531963af
Nic Benders <nic.benders@gmail.com>
2010-01-19 00:16:54 -0800

    UI Tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def user_time(time)
+    time.in_time_zone(current_user.time_zone)
+  end
+  
+  def pretty_time(time)
+    time.strftime("%b %d, %Y %H:%M %Z")
+  end
 end
