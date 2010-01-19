c2649079d56a7d7c930ce32fd5e4c777746884f0
Nic Benders <nic.benders@gmail.com>
2010-01-19 00:16:49 -0800

    Polling system

--- lib/workers/alert_worker.rb
+++ lib/workers/alert_worker.rb
@@ -2,31 +2,41 @@ class AlertWorker < BackgrounDRb::MetaWorker
   set_worker_name :alert_worker
 
   def create(args = nil)
-    update_notifications ; add_periodic_timer(31.minutes) { update_notifications }
-    update_mail_messages ; add_periodic_timer(31.minutes) { update_mail_messages }
+    update_characters ; add_periodic_timer(61.minutes) { update_characters }
+    update_alerts ; add_periodic_timer(31.minutes) { update_alerts }
   end
   
-  def update_notifications
-    logger.info "Started Notification job"
-    Character.find(:all, :conditions => "account_id IS NOT NULL").each do |character|
-      character.if_stale(character.account.id, character.account.api.personal_notification_url, {:characterid => character.id}, 30.minutes) do
-        character.account.api.personal_notifications(:characterid => character.id).each do |x|
-          x.send_alert! unless x.read
-          character.notifications.create(x.instance_values) unless character.notifications.exists?(x.id)
+  def update_characters
+    logger.info "Started Characters Job"
+    Account.all.each do |account|
+      logger.debug "Getting Characters for #{account.id}"
+      account.get_characters
+    end
+  end
+  
+  def update_alerts
+    logger.info "Started Alerts job"
+    Account.all.each do |account|
+      account.characters.each do |character|
+        on_new(character, 'notifications') do |n|
+          logger.info "New notification ##{n.id} for #{n.character.name}"
+        end
+
+        on_new(character, 'mail_messages') do |m|
+          logger.info "New message ##{m.id} for #{m.character.name}"
         end
       end
     end
   end
-  
-  def update_mail_messages
-    logger.info "Started MailMessage job"
-    Character.find(:all, :conditions => "account_id IS NOT NULL").each do |character|
-      character.if_stale(character.account.id, character.account.api.personal_mail_messages_url, {:characterid => character.id}, 30.minutes) do
-        character.account.api.personal_mail_messages(:characterid => character.id).each do |x|
-          x.send_alert! unless x.read
-          character.mail_messages.create(x.instance_values) unless character.mail_messages.exists?(x.id)
-        end
-      end
+
+  private
+
+  def on_new(object, type, &block)
+    logger.debug "Looking for #{type} on #{object.class.to_s}"
+    max_id = object.send(type).maximum(:id)
+    messages = object.send("get_" + type)
+    messages.select{|x| x.id > max_id && !x.read}.each do |m|
+      yield(m)
     end
   end
 end
