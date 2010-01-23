2687063aea4c7d3595aa5d2158da6d7216afd0b0
Nic Benders <nic.benders@gmail.com>
2010-01-22 21:40:19 -0800

    Initial working BackgrounDRb and Twitter

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,11 @@
+require 'twitter'
+
 module UserHelper
+  def following_alerts?
+    @@httpauth ||= Twitter::HTTPAuth.new(TWITTER_LOGIN, TWITTER_PASSWORD)
+    @@twitter_client ||= Twitter::Base.new(@@httpauth)
+    @@alert_user_id ||= @@twitter_client.verify_credentials.id
+
+    current_user.twitter.get('/friends/ids.json').include?(@@alert_user_id)
+  end
 end
--- lib/workers/alert_worker.rb
+++ lib/workers/alert_worker.rb
@@ -1,7 +1,12 @@
+require 'twitter'
+
 class AlertWorker < BackgrounDRb::MetaWorker
   set_worker_name :alert_worker
 
   def create(args = nil)
+    httpauth = Twitter::HTTPAuth.new(TWITTER_LOGIN, TWITTER_PASSWORD)
+    @twitter_client = Twitter::Base.new(httpauth)
+    
     update_characters ; add_periodic_timer(61.minutes) { update_characters }
     update_alerts ; add_periodic_timer(31.minutes) { update_alerts }
   end
@@ -9,7 +14,7 @@ class AlertWorker < BackgrounDRb::MetaWorker
   def update_characters
     logger.info "Started Characters Job"
     Account.all.each do |account|
-      logger.debug "Getting Characters for #{account.id}"
+      logger.debug "Getting Characters for Account #{account.id}"
       account.get_characters
     end
   end
@@ -20,10 +25,12 @@ class AlertWorker < BackgrounDRb::MetaWorker
       account.characters.each do |character|
         on_new(character, 'notifications') do |n|
           logger.info "New notification ##{n.id} for #{n.character.name}"
+          alert(n)
         end
 
         on_new(character, 'mail_messages') do |m|
           logger.info "New message ##{m.id} for #{m.character.name}"
+          alert(m)
         end
       end
     end
@@ -32,11 +39,34 @@ class AlertWorker < BackgrounDRb::MetaWorker
   private
 
   def on_new(object, type, &block)
-    logger.debug "Looking for #{type} on #{object.class.to_s}"
+    logger.debug "Looking for #{type} on #{object.class.to_s} #{object.id}"
     max_id = object.send(type).maximum(:id)
     messages = object.send("get_" + type)
-    messages.select{|x| x.id > max_id && !x.read}.each do |m|
+    messages.select{|x| x.class.to_s == type.classify && x.id > max_id && !x.read}.each do |m|
       yield(m)
     end
+  end
+  
+  def alert(subject)
+    text = case subject
+    when Notification
+      alert_notification_text(subject)
+    when MailMessage
+      alert_mail_message_text(subject)
+    end
+    
+    begin
+      @twitter_client.direct_message_create(subject.character.account.user.twitter_id, text[0..139])
+    rescue Twitter::General => e
+      logger.info "Got error sending DM to @#{subject.character.account.user.login}: #{e.message}"
+    end
+  end
+  
+  def alert_notification_text(n)
+    "Eve Notification for #{n.character.name} - #{NotificationType.get(n.notification_type_id)}"
+  end
+  
+  def alert_mail_message_text(m)
+    "Eve Message for #{m.character.name} - #{m.title}"
   end
 end
