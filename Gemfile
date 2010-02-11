d2b21c07f1ceec847acf222a3982a71c59883454
Nic Benders <nic.benders@gmail.com>
2010-02-10 22:55:58 -0800

    Working Gemfile

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ source :gemcutter
 
 gem "rails", "3.0.0.beta"
 gem "reve", :git => "git://github.com/lisa/reve.git"
-gem "sqlite3-ruby"
-gem "twitter-auth"
+gem "sqlite3-ruby", :require => "sqlite3"
+gem "twitter-auth", "0.1.22", :require => "twitter_auth"
 gem "json"
 gem "chronic"
 gem "packet"
---
+++ config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = 'bb88f2079d545136410c6a17371860c7d0bae93673a85cabe1fc05361a786e44323bc4ad7a5daf1fe2ea047d0e1de2b253b19e654147a19a4987a6fece541f88'
