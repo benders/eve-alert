53f9c9b5c43c6d5f8fe27a2211e1c0d1fe098d06
Nic Benders <nic.benders@gmail.com>
2010-02-11 16:36:14 -0800

    Simplify User route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 EveAlert::Application.routes.draw do
-  match 'user/:action/:id' => 'user#index', :as => :home
+  match 'user' => 'user#index', :as => :home
   resources :accounts do
       resources :characters
   end
